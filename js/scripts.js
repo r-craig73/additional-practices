37738365c85cd765ebeee28010f98af823cdd16e
Ron Craig <ron.craig@comcast.net>
2018-08-23 22:22:10 -0700

    edit JS notation and alert windows; include three digit precision to BMI

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-var myHeight = parseInt(prompt("What's your height, in inches:"));
-var myWeight = parseInt(prompt("What's your weight (be honest), in lbs:"));
+var myHeight = parseInt(prompt("What is your height, in inches?"));
+var myWeight = parseInt(prompt("What is your weight (be honest), in lbs?"));
 
-var bodyMassIndex = function(weightPounds, heightInches) {return "Your BMI is " + (703 * weightPounds) / (heightInches * heightInches);
+var bodyMassIndex = function(weightPounds, heightInches) {
+  return "Your BMI is " + ((703 * weightPounds) / (heightInches * heightInches)).toPrecision(3);
 };
 
 alert(bodyMassIndex(myWeight, myHeight));
